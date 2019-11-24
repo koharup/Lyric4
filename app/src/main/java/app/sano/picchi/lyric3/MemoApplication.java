2f88f55876915106893992f00c6bceaa24a94398
koharup <anandpita18@icloud.com>
2019-11-24 19:10:34 +0900

    詳細画面に歌詞1と曲名、日にち、アーティスト表示 デザインと最初の画面なし

---
+++ app/src/main/java/app/sano/picchi/lyric3/MemoApplication.java
@@ -0,0 +1,15 @@
+package app.sano.picchi.lyric3;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class MemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(getApplicationContext());
+    }
+
+}
---
+++ app/src/main/java/app/sano/picchi/lyric3/TranslationActivity.kt
@@ -0,0 +1,53 @@
+package app.sano.picchi.lyric3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_translation.*
+
+class TranslationActivity : AppCompatActivity() {
+
+    //realm型の変数を宣言
+    lateinit var realm: Realm
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_translation)
+
+        //realmを開く
+        Realm.init(this)
+        realm = Realm.getDefaultInstance()
+
+        showData()
+
+    }
+
+    fun showData() {
+        val memo = realm.where(Memo::class.java).equalTo(
+            "updateDate",
+            intent.getStringExtra("updateDate")
+        ).findFirst()
+
+        titleText.setText(memo.title)
+        contentText.setText(memo.content)
+        updateText.setText(memo.updateDate)
+        kashiText.setText(memo.word1)
+
+    }
+
+    fun ChangeActivity(v: View) {
+        val intent = Intent(this, Translation2Activity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        //realmを閉じる
+        realm.close()
+    }
+}
